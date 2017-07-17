1f05210bd718b0b1f071affa6c46cf28fed9e1d1
Michael Josephson <mikej@noreply.users.github.com>
2017-07-17 08:35:56 +0100

    JSX

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const ce = React.createElement;
-
 const MyTitle = function(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
-};
-
-const MyFirstComponent = function() {
-  return ce(
-    'div',
-    null,
-    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
-    ce(MyTitle, { title: 'Silicon Valley', color: 'peru' })
+  return (
+    <div>
+      <h1 style={{ color: props.color }}>
+        {props.title}
+      </h1>
+    </div>
   );
 };
 
-render(ce(MyFirstComponent), document.getElementById('app'));
+const MyFirstComponent = function() {
+  return (
+    <div id="my-first-component">
+      <MyTitle title="Game of Thrones" color="YellowGreen" />
+      <MyTitle title="Stranger Things" color="GreenYellow" />
+      <MyTitle title="House of Cards" color="Peru" />
+      <MyTitle title="The Americans" color="burlywood" />
+    </div>
+  );
+};
+
+render(<MyFirstComponent />, document.getElementById('app'));

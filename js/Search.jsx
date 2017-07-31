f0937d91384609199beeda94a93f7dd08a2ad913
Michael Josephson <mikej@users.noreply.github.com>
2017-07-31 09:03:49 +0100

    Filtering

--- js/Search.jsx
+++ js/Search.jsx
@@ -19,7 +19,9 @@ class Search extends Component {
           <input onChange={this.handleSearchTermChange} value={this.state.searchTerm} type="text" placeholder="Search" />
         </header>
         <div>
-          {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+          {preload.shows
+            .filter(show => `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+            .map(show => <ShowCard key={show.imdbID} {...show} />)}
         </div>
       </div>
     );

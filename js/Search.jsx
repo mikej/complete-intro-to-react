c967d7833e08782a7747095f1a73193191161566
Michael Josephson <mikej@users.noreply.github.com>
2018-02-25 18:12:30 +0000

    display more details (show cards)

--- js/Search.jsx
+++ js/Search.jsx
@@ -3,7 +3,16 @@ import preload from '../data.json';
 
 const Search = () => (
   <div className="search">
-    <pre>{preload.shows.map(show => <h3>{show.title}</h3>)}</pre>
+    {preload.shows.map(show => (
+      <div className="show-card">
+        <img alt={`${show.title} show poster`} src={`/public/img/posters/${show.poster}`} />
+        <div>
+          <h3>{show.title}</h3>
+          <h4>({show.year})</h4>
+          <p>{show.description}</p>
+        </div>
+      </div>
+    ))}
   </div>
 );
 

2258a00b1fc27bb4ac1db6b3849914e7b1df5517
Michael Josephson <mikej@users.noreply.github.com>
2018-03-03 11:42:16 +0000

    URL Parameters - add intial Details component

--- js/App.jsx
+++ js/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './Landing';
 import Search from './Search';
+import Details from './Details';
+import preload from '../data.json';
 
 const FourOhFour = () => <h1>404</h1>;
 
@@ -11,6 +13,13 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route path="/search" component={Search} />
+        <Route
+          path="/details/:id"
+          component={props => {
+            const selectedShow = preload.shows.find(show => props.match.params.id === show.imdbID);
+            return <Details show={selectedShow} {...props} />;
+          }}
+        />
         <Route component={FourOhFour} />
       </Switch>
     </div>

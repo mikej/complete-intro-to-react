ff409edd7467f27d7b72641f8e6470a4e5d64671
Michael Josephson <mikej@users.noreply.github.com>
2018-03-03 11:57:50 +0000

    add Flow to App.jsx

--- js/App.jsx
+++ js/App.jsx
@@ -1,5 +1,8 @@
+// @flow
+
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import type { Match } from 'react-router-dom';
 import Landing from './Landing';
 import Search from './Search';
 import Details from './Details';
@@ -15,7 +18,7 @@ const App = () => (
         <Route path="/search" component={Search} />
         <Route
           path="/details/:id"
-          component={props => {
+          component={(props: { match: Match }) => {
             const selectedShow = preload.shows.find(show => props.match.params.id === show.imdbID);
             return <Details show={selectedShow} {...props} />;
           }}

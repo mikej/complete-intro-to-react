9fe53fd2cef6cba0e36203d88626f6a9f2ba636a
Michael Josephson <mikej@users.noreply.github.com>
2018-03-10 20:54:25 +0000

    Dispatching Actions in tests

--- js/__tests__/Search.spec.jsx
+++ js/__tests__/Search.spec.jsx
@@ -1,7 +1,11 @@
 // @flow
 
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, render } from 'enzyme';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import store from '../store';
+import { setSearchTerm } from '../actionCreators';
 import preload from '../../data.json';
 import Search, { Unwrapped as UnwrappedSearch } from '../Search';
 import ShowCard from '../ShowCard';
@@ -16,7 +20,7 @@ test('Search should render correct amount of shows', () => {
   expect(component.find(ShowCard).length).toEqual(preload.shows.length);
 });
 
-test('Search should render correct amount of shows based on search term', () => {
+test('Search should render correct amount of shows based on search term - without Redux', () => {
   const searchWord = 'black';
   const component = shallow(<UnwrappedSearch shows={preload.shows} searchTerm={searchWord} />);
   // component.find('input').simulate('change', { target: { value: searchWord } });
@@ -25,4 +29,21 @@ test('Search should render correct amount of shows based on search term', () => 
   ).length;
 
   expect(component.find(ShowCard).length).toEqual(showCount);
+});
+
+test('Search should render correct amount of shows based on search term - with Redux', () => {
+  const searchWord = 'black';
+  store.dispatch(setSearchTerm(searchWord));
+  const component = render(
+    <Provider store={store}>
+      <MemoryRouter>
+        <Search shows={preload.shows} />
+      </MemoryRouter>
+    </Provider>
+  );
+  const showCount = preload.shows.filter(
+    show => `${show.title} ${show.description}`.toUpperCase().indexOf(searchWord.toUpperCase()) >= 0
+  ).length;
+
+  expect(component.find('.show-card').length).toEqual(showCount);
 });
